7449ca82ee430fe4767874503251ffb1817d2cd9
cryptondr <83161554+cryptondr@users.noreply.github.com>
2022-05-10 18:40:05 +0200

    adding storeapp, customizing admin page and making dynamic products display in homepage

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA FILES CONF
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
