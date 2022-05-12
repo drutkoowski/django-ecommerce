a05a9704436c101db4edf400951cc0878653c80a
cryptondr <83161554+cryptondr@users.noreply.github.com>
2022-05-12 23:26:35 +0200

    creating signup view, registration forms and logic with django alerts

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -137,3 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA FILES CONF
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
