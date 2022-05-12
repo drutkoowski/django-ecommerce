940a9cc0f2d918320d560c23e59cdb07a9f3c9d2
cryptondr <83161554+cryptondr@users.noreply.github.com>
2022-05-12 15:54:23 +0200

    adding search functionality and size/color variations

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
-from carts.models import Cart, CartItem
+from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -18,7 +19,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by("id")
         paginator = Paginator(products, per_page=2)
         page = request.GET.get("page")
         paged_products = paginator.get_page(page)
@@ -42,3 +43,16 @@ def product_detail(request, category_slug, product_slug):
         "is_in_cart": is_in_cart,
     }
     return render(request, "store/product_detail.html", context=context)
+
+
+def search(request):
+    if "keyword" in request.GET:
+        keyword = request.GET["keyword"]
+        if keyword:
+            products = Product.objects.order_by("-create_date").filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            products_count = products.count()
+    context = {
+        "products": products,
+        "product_count": products_count,
+    }
+    return render(request, "store/store.html", context)
