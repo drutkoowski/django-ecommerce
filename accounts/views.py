c093058de34da322776e01e3d246cb3cadc2bdba
cryptondr <83161554+cryptondr@users.noreply.github.com>
2022-05-17 23:40:43 +0200

    adding dashboard functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
-
-from .forms import RegistrationForm
-from accounts.models import Account
+from orders.models import Order
+from .forms import RegistrationForm, UserForm, UserProfileForm
+from accounts.models import Account, UserProfile
 from django.contrib import messages, auth
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
 import requests
+
+
 # Create your views here.
 
 def register(request):
@@ -32,6 +34,12 @@ def register(request):
                            password=password, username=username)
             user.phone_number = phone_number
             user.save()
+
+            # create user profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = "default/default_user.png"
+            profile.save()
 
             current_site = get_current_site(request)
             mail_subject = "Please activate your account"
@@ -103,7 +111,7 @@ def login(request):
             url = request.META.get('HTTP_REFERER')
             try:
                 query = requests.utils.urlparse(url).query
-                params = dict(x.split("=")for x in query.split("&"))
+                params = dict(x.split("=") for x in query.split("&"))
                 if 'next' in params:
                     nextPage = params['next']
                     return redirect(nextPage)
@@ -142,7 +150,12 @@ def activate(request, uidb64, token):
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, "accounts/dashboard.html")
+    orders = Order.objects.order_by("-created_at").filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+    context = {
+        "orders_count": orders_count,
+    }
+    return render(request, "accounts/dashboard.html", context)
 
 
 def forgotPassword(request):
@@ -203,3 +216,40 @@ def resetPassword(request):
             return redirect("resetPassword")
     else:
         return render(request, "accounts/resetPassword.html")
+
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by("-created_at")
+    context = {
+        "orders": orders,
+    }
+    return render(request, "accounts/my_orders.html", context)
+
+
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+        "userprofile": userprofile,
+    }
+
+    return render(request, "accounts/edit_profile.html", context)
+
+
+def change_password(request):
+    if request.method == "POST":
+        current_password = request.POST['current_password']
+    return render(request, "accounts/change_password.html")
