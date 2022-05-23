3b920f6174171b3972468f8fee01a5a9b3a57fa3
cryptondr <83161554+cryptondr@users.noreply.github.com>
2022-05-23 17:17:34 +0200

    updating filters on home page products

--- djangoecommerce/views.py
+++ djangoecommerce/views.py
@@ -4,7 +4,7 @@ from store.models import Product, ReviewRating
 
 def home(request):
     reviews = None
-    products = Product.objects.all().filter(is_available=True).order_by("modified_date", "price")
+    products = Product.objects.all().filter(is_available=True).order_by("price")
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
     context = {
