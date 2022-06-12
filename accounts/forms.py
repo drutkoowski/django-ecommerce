028e5de88c0aed8e2b920ccc9fbfa22b34866337
drutkoowski <d.rutkowski2000@gmail.com>
2022-06-13 01:24:32 +0200

    fixing bugs

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,7 +15,7 @@ class RegistrationForm(forms.ModelForm):
 
     class Meta:
         model = Account
-        fields = ["first_name", "last_name", "phone_number", "email", "password", "confirm_password", 'username']
+        fields = ["first_name", "last_name", "phone_number", "email", "password", "confirm_password"]
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
